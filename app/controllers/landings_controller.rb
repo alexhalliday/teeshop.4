d621ef36e6b9abe0f8377b8703012e58155b868c
Alex Halliday <alex.halliday@teespring.com>
2015-06-05 17:10:58 -0700

    Reorganized files and made /choose_site for users to pick their storefront when they first login.

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -3,6 +3,11 @@ class LandingsController < ApplicationController
   #->Prelang (scaffolding:rails/scope_to_user)
   before_filter :require_user_signed_in
 
+   layout "basic", only: [:choose_site]
+
   def index
+  end
+
+  def choose_site
   end
 end
